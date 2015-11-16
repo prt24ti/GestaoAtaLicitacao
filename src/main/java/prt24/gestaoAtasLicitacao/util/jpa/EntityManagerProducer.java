afa8fdfe3c360912754f7aac79123d4561efd211
joao <jpfguedes@gmail.com>
2015-11-16 09:09:40 -0300

    Commit com mais mudanças nos pacotes java e no template xhtml.

---
+++ src/main/java/prt24/gestaoAtasLicitacao/util/jpa/EntityManagerProducer.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package prt24.gestaoAtasLicitacao.util.jpa;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * @author joao
+ *
+ */
+
+@ApplicationScoped
+public class EntityManagerProducer {
+	
+	private EntityManagerFactory factory;
+	
+	public EntityManagerProducer() {
+		factory = Persistence.createEntityManagerFactory("PedidoPU");
+	}
+	
+	@Produces @RequestScoped
+	public EntityManager createEntityManager() {
+		return factory.createEntityManager();
+	}
+	
+	public void closeEntityManager(@Disposes EntityManager manager) {
+		manager.close();
+	}
+}
